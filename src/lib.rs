805496db2016c191e27fb1763add99c45f8b4058
Koen Bolhuis <koen.bolhuis@gmail.com>
2019-04-19 19:33:29 +0200

    Add one_or_more and zero_or_more combinators

--- src/lib.rs
+++ src/lib.rs
@@ -50,8 +50,7 @@ fn identifier(input: &str) -> ParseResult<String> {
     Ok((&input[next_index..], matched))
 }
 
-fn pair<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2)
-    -> impl Parser<'a, (R1, R2)>
+fn pair<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Parser<'a, (R1, R2)>
 where
     P1: Parser<'a, R1>,
     P2: Parser<'a, R2>,
@@ -64,8 +63,7 @@ where
     }
 }
 
-fn map<'a, P, F, A, B>(parser: P, map_fn: F)
-    -> impl Parser<'a, B>
+fn map<'a, P, F, A, B>(parser: P, map_fn: F) -> impl Parser<'a, B>
 where
     P: Parser<'a, A>,
     F: Fn(A) -> B,
@@ -75,8 +73,7 @@ where
             .map(|(next_input, result)| (next_input, map_fn(result)))
 }
 
-fn left<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2)
-    -> impl Parser<'a, R1>
+fn left<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Parser<'a, R1>
 where
     P1: Parser<'a, R1>,
     P2: Parser<'a, R2>,
@@ -84,13 +81,51 @@ where
     map(pair(parser1, parser2), |(left, _right)| left)
 }
 
-fn right<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2)
-    -> impl Parser<'a, R2>
+fn right<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Parser<'a, R2>
 where
     P1: Parser<'a, R1>,
     P2: Parser<'a, R2>,
 {
     map(pair(parser1, parser2), |(_left, right)| right)
+}
+
+fn one_or_more<'a, P, A>(parser: P) -> impl Parser<'a, Vec<A>>
+where
+    P: Parser<'a, A>,
+{
+    move |mut input| {
+        let mut result = Vec::new();
+
+        if let Ok((next_input, first_item)) = parser.parse(input) {
+            input = next_input;
+            result.push(first_item);
+        } else {
+            return Err(input);
+        }
+
+        while let Ok((next_input, next_item)) = parser.parse(input) {
+            input = next_input;
+            result.push(next_item);
+        }
+
+        Ok((input, result))
+    }
+}
+
+fn zero_or_more<'a, P, A>(parser: P) -> impl Parser<'a, Vec<A>>
+where
+    P: Parser<'a, A>
+{
+    move |mut input| {
+        let mut result = Vec::new();
+
+        while let Ok((next_input, next_item)) = parser.parse(input) {
+            input = next_input;
+            result.push(next_item);
+        }
+
+        Ok((input, result))
+    }
 }
 
 #[cfg(test)]
@@ -150,5 +185,33 @@ mod tests {
         );
         assert_eq!(Err("oops"), tag_opener.parse("oops"));
         assert_eq!(Err("!oops"), tag_opener.parse("<!oops"));
+    }
+
+    #[test]
+    fn one_or_more_combinator() {
+        let parser = one_or_more(match_literal("ha"));
+        assert_eq!(
+            Ok(("", vec![(), (), ()])),
+            parser.parse("hahaha")
+        );
+        assert_eq!(Err("ahah"), parser.parse("ahah"));
+        assert_eq!(Err(""), parser.parse(""));
+    }
+
+    #[test]
+    fn zero_or_more_combinator() {
+        let parser = zero_or_more(match_literal("ha"));
+        assert_eq!(
+            Ok(("", vec![(), (), ()])),
+            parser.parse("hahaha")
+        );
+        assert_eq!(
+            Ok(("ahah", vec![])),
+            parser.parse("ahah")
+        );
+        assert_eq!(
+            Ok(("", vec![])),
+            parser.parse("")
+        );
     }
 }
